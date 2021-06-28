009e93c0ab4340925462dc1ff2f1f4774bce15d9
Anurag Gupta <anuragsrmcse@gmail.com>
2021-06-28 16:18:45 +0530

    Added the number format exception

--- src/main/java/com/greatlearning/dice/utils/ValidationUtils.java
+++ src/main/java/com/greatlearning/dice/utils/ValidationUtils.java
@@ -6,11 +6,15 @@ import com.greatlearning.dice.exceptions.IncorrectInputException;
 public class ValidationUtils {
 
 	public static void validInput(String... args) throws IncorrectInputException {
-		if (!Optional.fromNullable(args).isPresent() || args.length != 2)
-			throw new IncorrectInputException("N and M are invalid");
-		if (Integer.parseInt(args[0]) < 2)
-			throw new IncorrectInputException("N should be at Least 2");
-		if (Integer.parseInt(args[1]) < 1)
-			throw new IncorrectInputException("M should be at Least 1");
+		try {
+			if (!Optional.fromNullable(args).isPresent() || args.length != 2)
+				throw new IncorrectInputException("N and M are invalid");
+			if (Integer.parseInt(args[0]) < 2)
+				throw new IncorrectInputException("N should be at Least 2");
+			if (Integer.parseInt(args[1]) < 1)
+				throw new IncorrectInputException("M should be at Least 1");
+		} catch (NumberFormatException ex) {
+			throw new IncorrectInputException("N and M are Not Integer");
+		}
 	}
 }
